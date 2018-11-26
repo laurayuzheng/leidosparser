46e2c6a84ab8e34aa511f0d9318db75f517f0015
Laura Zheng <laurazheng@wireless-10-104-51-191.umd.edu>
2018-11-26 17:56:36 -0500

    adapted to create files for all csv rows.

--- plaintext_parser.py
+++ plaintext_parser.py
@@ -3,24 +3,25 @@ import csv
 # reads the plaintext from the csv file.
 # filename is the csv file
 # writeto is the file the plaintext is written to.
+# returns integer, number of files created.
 def read_csv(filename, writeto):
 	with open(filename, mode='r') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
 		line_count = 0
-		file = open(writeto, "w+")
 		for row in csv_reader:
 			if line_count == 0:
 				#print(f'Column names are {", ".join(row)}')
 				line_count += 1
-			#print(f'\tsummaryText: {row["summaryText"]}')
-			#print(row["summaryText"])
-			#file.write(f'{row["summaryText"]}')
+			file = open(writeto + "%d.txt" % line_count, "w+")
 			file.write(row["summaryText"])
+			file.close()
+			print(f'Processed line {line_count}.')
 			line_count += 1
-		file.close()
-		print(f'Processed {line_count} lines.')
+		#print(f'Processed {line_count} lines.')
+		return line_count
 
 
+#def parse_text(file):
 
 if __name__ == "__main__":
-	read_csv('b_result.csv','b_result_plaintext.txt')
+	num_files = read_csv('b_result.csv','b_result_plaintext')
